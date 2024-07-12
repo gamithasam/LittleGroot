43be067d8f0b01d26674c7e70209a126a37a207c
gamithasam <gamitha@asia.com>
2024-07-12 22:17:46 +0530

    Updated the Database Dump 	modified:   ../Database/DatabaseDump.sql

--- Database/DatabaseDump.sql
+++ Database/DatabaseDump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `LittleGroot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `LittleGroot`;
 -- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
 --
 -- Host: localhost    Database: LittleGroot
@@ -24,4 +26,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-17  2:07:39
+-- Dump completed on 2024-07-12 22:16:04
